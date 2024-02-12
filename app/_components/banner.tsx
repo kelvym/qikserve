45d1db2c79459a1e7770d3e2fd9f07a5036f9f4d
Kelvym Miranda <kelvymm@gmail.com>
2024-02-12 07:01:39 -0300

    init

---
+++ app/_components/banner.tsx
@@ -0,0 +1,7 @@
+export const Banner = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-96 bg-gray-800">
+      <h1 className="text-4xl text-white">Welcome to the app!</h1>
+    </div>
+  );
+};
