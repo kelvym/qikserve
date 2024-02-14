3876c75c970a5401949ae2ba7023cd46cffecc2b
Kelvym Miranda <kelvymm@gmail.com>
2024-02-14 19:31:32 -0300

    Fix format issues

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,11 @@
 import createMiddleware from 'next-intl/middleware'
 
 export default createMiddleware({
-  // A list of all locales that are supported
   locales: ['en', 'pt-br'],
 
   defaultLocale: 'en',
 })
 
 export const config = {
-  // Match only internationalized pathnames
   matcher: ['/', '/(pt-br|en)/:path*'],
 }
